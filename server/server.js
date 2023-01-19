a7bdc74ebaf98db559ed3e07d04703ff7b535fc9
Anthony Maras <marasantonis@gmail.com>
2023-01-19 11:29:35 +0200

    add pizza app.use

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db/dbConnection');
 const userRoute = require('./routes/usersRoutes');
+const pizzasRoute = require('./routes/pizzasRoute');
 const port = 5000;
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/users/', userRoute);
+app.use('/api/pizzas/', pizzasRoute);
 
 const start = async () => {
   try {
