eb2a2779e6f693d38a306f4edf55d9aae5ce11a2
Anthony Maras <marasantonis@gmail.com>
2023-01-20 10:15:14 +0200

    update wip

--- app/online-restaurant/src/App.js
+++ app/online-restaurant/src/App.js
@@ -1,7 +1,11 @@
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
 function App() {
   return (
     <div className="App">
       <h1>online restaurant</h1>
+      {/* <LoginScreen /> */}
+      <RegisterScreen />
     </div>
   );
 }
--- app/online-restaurant/src/index.js
+++ app/online-restaurant/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+import './index.css';
 
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
