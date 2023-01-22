d2905e9d5a6cd161c3d9c33e23980a0e28f783a7
Anthony Maras <marasantonis@gmail.com>
2023-01-22 15:49:53 +0200

    fixed order route

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -5,12 +5,10 @@ const router = express.Router();
 router.post('/order', async (req, res) => {
   try {
     const { userId, orderItems } = req.body;
-
     const order = await Order.create({
       userId,
       orderItems,
     });
-
     res.status(201).json(order);
   } catch (error) {
     res.status(500).json(error);
@@ -19,7 +17,6 @@ router.post('/order', async (req, res) => {
 
 router.get('/list', async (req, res) => {
   const { userId } = req.query;
-
   try {
     const response = await Order.find({ userId });
     res.status(200).send(response);
